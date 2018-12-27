feaff8094786c4c15217039c855bc54beecb95b6
Emmanuel Mtesigwa <emtesigwa@CRDBBANKPLC.COM>
2018-12-27 14:24:17 +0300

    Homework #1

---
+++ song.js
@@ -0,0 +1,33 @@
+
+var songName = "Little Things";
+var singer = "One Direction";
+var songwritter = ["Fiona Bevan" , "Ed Sheeran"];
+var producer = "Jake Gosling";
+var boyBand = true;
+var released = "02 Nov 2012";
+var bandSingers = ["Harry Styles" , "Louis Tomlinson" , "Liam Payne" , "Zayn Malik" , "Niall Horan"];
+var songDurationInSeconds = 219;
+var composition = {
+	key : "G major" ,
+	vocal_range : "A3 to D5" , 
+	instrumentation : ["guitar string" , "piano lines" , "vocals"]
+};
+var video = {
+	director : "Vaughan Arnell" , 
+	shot_type : "black and white" ,
+	production_date : "15 oct 2012"
+
+};
+var performances = ["X FACTOR USA" , "X FACTOR UK" , "BBC's CHILDREN IN NEED 2012" , "BRITISH TV SERIES SURPRISE" , "ROYAL VARIETY PERFORMANCE" , "X FACTOR AUSTRALIA" , "TAKE ME HOME TOUR" , "WHERE WE ARE TOUR" , "ON ROAD AGAIN TOUR"];
+
+console.log(songName);
+console.log(singer);
+console.log(songwriter);
+console.log(producer);
+console.log(boyBand);
+console.log(released);
+console.log(bandSingers);
+console.log(songDurationInSeconds);
+console.log(composition);
+console.log(video);
+console.log(performances);
